1ddb57ad840754c5fc718ac4dbd39cadc42d6461
Dennis Ariel <dariel_504@hotmail.com>
2021-07-11 12:59:54 -0500

    diseño interfaz aplicacion

---
+++ src/proyecto_compilador/Proyecto_compilador.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyecto_compilador;
+
+/**
+ *
+ * @author dennis
+ */
+public class Proyecto_compilador {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
